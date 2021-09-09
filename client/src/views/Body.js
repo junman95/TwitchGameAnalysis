c8262a72cb1da7957b8c2b40f03aab690d7bea9f
junman95 <junman95@naver.com>
2021-09-09 23:22:14 +0900

    [Update] GridItems react on hover actions blur->clear [Add] StreamingView Component to GridItem Left Top

--- client/src/views/Body.js
+++ client/src/views/Body.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useState } from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
+//GridItems
+import StreamingView from "./gridItems/StreamingView";
 
 const Body = (props) => {
+
+  const [isLeftTopOpacity,setIsLeftTopOpacity] = useState("50%");
+  const [isLeftBottomOpacity,setIsLeftBottomOpacity] = useState("50%");
+  const [isCenterOpacity,setIsCenterOpacity] = useState("50%");
+  const [isRightTopOpacity,setIsRightTopOpacity] = useState("50%");
+  const [isRightBottomOpacity,setIsRightBottomOpacity] = useState("50%");
+
   return (
     <Grid
       margin="7"
@@ -10,11 +20,13 @@ const Body = (props) => {
       templateColumns="repeat(5, 1fr)"
       gap={3}
     >
-      <GridItem colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={"50%"}>hi</GridItem>
-      <GridItem colSpan={3} rowSpan={2} bg={'#A0AEC0'} opacity={"50%"}>hi</GridItem>
-      <GridItem colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={"50%"}>hi</GridItem>
-      <GridItem colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={"50%"}>hi</GridItem>
-      <GridItem colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={"50%"}>hi</GridItem>
+      <GridItem onMouseOver={()=>setIsLeftTopOpacity("100%")} onMouseOut={()=>setIsLeftTopOpacity("50%")} colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isLeftTopOpacity}></GridItem>
+      <GridItem onMouseOver={()=>setIsCenterOpacity("100%")} onMouseOut={()=>setIsCenterOpacity("50%")} colSpan={3} rowSpan={2} bg={'#A0AEC0'} opacity={isCenterOpacity}>
+        <StreamingView/>
+      </GridItem>
+      <GridItem onMouseOver={()=>setIsRightTopOpacity("100%")} onMouseOut={()=>setIsRightTopOpacity("50%")} colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isRightTopOpacity}>hi</GridItem>
+      <GridItem onMouseOver={()=>setIsLeftBottomOpacity("100%")} onMouseOut={()=>setIsLeftBottomOpacity("50%")} colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isLeftBottomOpacity}>hi</GridItem>
+      <GridItem onMouseOver={()=>setIsRightBottomOpacity("100%")} onMouseOut={()=>setIsRightBottomOpacity("50%")} colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isRightBottomOpacity}>hi</GridItem>
     </Grid>
   );
 };
---
+++ client/src/views/gridItems/StreamingView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const StreamingView = props => {
+  return (
+    <div>
+      StreamingView
+    </div>
+  );
+};
+
+StreamingView.propTypes = {
+  
+};
+
+export default StreamingView;
