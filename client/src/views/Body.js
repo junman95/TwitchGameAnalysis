cd09c8d6924e31d13b3fe107fc6b52448a46d97a
junman95 <junman95@naver.com>
2021-09-11 01:20:12 +0900

    [Update] RankingView connection with Twitch game ranking api listed game rankings into the body insert ranking icon

--- client/src/views/Body.js
+++ client/src/views/Body.js
@@ -3,30 +3,32 @@ import { useState } from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
 //GridItems
 import StreamingView from "./gridItems/StreamingView";
+import RankingView from "./gridItems/RankingView";
 
 const Body = (props) => {
 
-  const [isLeftTopOpacity,setIsLeftTopOpacity] = useState("50%");
-  const [isLeftBottomOpacity,setIsLeftBottomOpacity] = useState("50%");
-  const [isCenterOpacity,setIsCenterOpacity] = useState("50%");
-  const [isRightTopOpacity,setIsRightTopOpacity] = useState("50%");
-  const [isRightBottomOpacity,setIsRightBottomOpacity] = useState("50%");
+  const [isLeftTopOpacity,setIsLeftTopOpacity] = useState("80%");
+  const [isLeftBottomOpacity,setIsLeftBottomOpacity] = useState("80%");
+  const [isCenterOpacity,setIsCenterOpacity] = useState("80%");
+  const [isRightTopOpacity,setIsRightTopOpacity] = useState("80%");
+  const [isRightBottomOpacity,setIsRightBottomOpacity] = useState("80%");
 
   return (
     <Grid
       margin="7"
       h="850"
       templateRows="repeat(2, 1fr)"
-      templateColumns="repeat(5, 1fr)"
+      templateColumns="repeat(4, 1fr)"
       gap={3}
     >
-      <GridItem onMouseOver={()=>setIsLeftTopOpacity("100%")} onMouseOut={()=>setIsLeftTopOpacity("50%")} colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isLeftTopOpacity}></GridItem>
-      <GridItem onMouseOver={()=>setIsCenterOpacity("100%")} onMouseOut={()=>setIsCenterOpacity("50%")} colSpan={3} rowSpan={2} bg={'#A0AEC0'} opacity={isCenterOpacity}>
+      <GridItem onMouseOver={()=>setIsLeftTopOpacity("100%")} onMouseOut={()=>setIsLeftTopOpacity("80%")} colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isLeftTopOpacity}>
+      </GridItem>
+      <GridItem colSpan={2} rowSpan={2} bg={'#A0AEC0'} opacity={isCenterOpacity}>
         <StreamingView/>
       </GridItem>
-      <GridItem onMouseOver={()=>setIsRightTopOpacity("100%")} onMouseOut={()=>setIsRightTopOpacity("50%")} colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isRightTopOpacity}>hi</GridItem>
-      <GridItem onMouseOver={()=>setIsLeftBottomOpacity("100%")} onMouseOut={()=>setIsLeftBottomOpacity("50%")} colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isLeftBottomOpacity}>hi</GridItem>
-      <GridItem onMouseOver={()=>setIsRightBottomOpacity("100%")} onMouseOut={()=>setIsRightBottomOpacity("50%")} colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isRightBottomOpacity}>hi</GridItem>
+      <GridItem onMouseOver={()=>setIsRightTopOpacity("100%")} onMouseOut={()=>setIsRightTopOpacity("80%")} colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isRightTopOpacity}><RankingView/></GridItem>
+      <GridItem colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isLeftBottomOpacity}>hi</GridItem>
+      <GridItem colSpan={1} rowSpan={1} bg={'#A0AEC0'} opacity={isRightBottomOpacity}>hi</GridItem>
     </Grid>
   );
 };
